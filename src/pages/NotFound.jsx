3ea09fa205777e6c2d6e4d5227b88cd8ad83ca2a
leeenah <76856144+leeenah@users.noreply.github.com>
2021-06-05 18:42:14 -0700

    Added inline CSS style in NotFound component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 export default function NotFound() {
+  const divStyle = {
+    textAlign: "center",
+    paddingTop: 30,
+  };
+
   return (
-    <div className="NotFound">
-      <h2> 404 Error! Page not found *sad face* </h2>
+    <div className="NotFound" style={divStyle}>
+      <h2> 404 Error! Page not found 😅 </h2>
     </div>
   );
 }
